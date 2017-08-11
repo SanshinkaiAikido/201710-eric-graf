0c940ee7f7630134891ad97291a9aa76f5f6db89
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2017-08-11 15:58:45 +0200

    Refactored and added PDF

--- process.sh
+++ process.sh
@@ -3,9 +3,18 @@ do
 	inkscape -f $i -w 842 -h 1191 -e `basename $i svg`png
 
 done
-for i in facebook_and_website.svg instagram.svg linkedin.svg
+for i in 20*svg
+do
+	inkscape -f $i ---export-pdf `basename $i svg`pdf
+
+done
+for i in 20*facebook_and_website.svg 20*instagram.svg 20*linkedin.svg
 do
 	inkscape -f $i -e `basename $i svg`png
 
 done
-convert instagram.png instagram.jpg
+for i in 20*instagram.png
+do
+	convert $i `basename $i png`jpg
+
+done
