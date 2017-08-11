6f0872762b27d77f89493b1569439311d4392d37
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2017-08-11 16:04:02 +0200

    Fixed PDF generation

--- process.sh
+++ process.sh
@@ -5,8 +5,10 @@ do
 done
 for i in 20*svg
 do
-	inkscape -f $i ---export-pdf `basename $i svg`pdf
-
+    if [ `echo $i|grep -c facebook` = 0 -a `echo $i|grep -c linkedin` = 0 -a `echo $i|grep -c instagram` = 0 ]
+    then
+    	inkscape -f $i --export-pdf `basename $i svg`pdf
+	fi
 done
 for i in 20*facebook_and_website.svg 20*instagram.svg 20*linkedin.svg
 do
