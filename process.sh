2ca6e5de9845a3ca2ac468fa12565c780f14e26c
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2017-08-11 15:52:02 +0200

    First official version

--- process.sh
+++ process.sh
@@ -3,9 +3,9 @@ do
 	inkscape -f $i -w 842 -h 1191 -e `basename $i svg`png
 
 done
-for i in facebook.svg instagram.svg linkedin.svg
+for i in facebook_and_website.svg instagram.svg linkedin.svg
 do
 	inkscape -f $i -e `basename $i svg`png
 
 done
-convert instagram.png instragram.jpg
+convert instagram.png instagram.jpg
